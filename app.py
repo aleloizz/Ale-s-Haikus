0d27a822d00ccaf010abb9d522c249e14f4a0811
aleloizz <aloizzo.1se@gmail.com>
2024-12-12 15:48:59 +0100

    Add sitemap route

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('static', 'sitemap.xml')
 
 @app.before_request
 def enforce_https_and_www():
